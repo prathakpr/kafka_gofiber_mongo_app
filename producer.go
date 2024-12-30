643b6efc39b8a38b61be7fcf0816d31a85b449c4
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-30 13:07:53 +0530

    added producer for the kafka

---
+++ producer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+	"github.com/segmentio/kafka-go"
+)
+
+func produceToKafka(message string) error {
+	writer := kafka.Writer{
+		Addr:     kafka.TCP("localhost:9092"),
+		Topic:    "test-topic",
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	defer writer.Close()
+
+	err := writer.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key"),
+			Value: []byte(message),
+		},
+	)
+
+	if err != nil {
+		log.Println("Failed to write message:", err)
+		return err
+	}
+
+	log.Println("Message sent to Kafka:", message)
+	return nil
+}
